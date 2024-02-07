b59954d3f12d6178b50322df54ee25915b780e30
Prithika19 <prithika087@gmail.com>
2024-02-07 12:18:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function brown(){
+     var x= document.getElementById("image").src="https://m.media-amazon.com/images/I/41KsVz81ZDL._AC_UL480_FMwebp_QL65_.jpg"
+     document.getElementById("price").innerHTML="Rs-$300"
+}
+
+function green(){
+    var x= document.getElementById("image").src="green.webp"
+    document.getElementById("price").innerHTML="Rs-$400"
+}
+
+function violet(){
+    var x= document.getElementById("image").src="voilet.webp"
+    document.getElementById("price").innerHTML="Rs-$150"
+}
+
+function black(){
+    var x= document.getElementById("image").src="striped.webp"
+    document.getElementById("price").innerHTML="Rs-$370"
+}
